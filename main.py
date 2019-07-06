69f7276f02574de6cbeee43c43ba6e0d4e5be67c
sheiss <sheissby@sina.com>
2019-07-06 21:01:37 +0800

    add

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from PyQt5 import QtWidgets
 # from demo import Ui_Dialog
@@ -18,12 +19,29 @@ class Search(QtWidgets.QWidget, Ui_Form):
     def __init__(self):
         super(Search, self).__init__()
         self.setupUi(self)
+        self.show_all()
 
     def search(self):
+        res = []
+        self.listWidget_2.clear()
         text = self.lineEdit.text()
+        for item in self.data:
+            if text in item:
+                res.append(item)
+        if len(res) != 0:
+            self.listWidget_2.addItems(res)
+        else:
+            self.listWidget_2.addItem('没有找到结果')
 
     def show_all(self):
-        self.tableView.
+        self.data = []
+        for _, _, files in os.walk(os.getcwd()+'\\tab'):
+            for file in files:
+                self.data.append(file)
+        # self.data = ['1','1','3','4','5']
+        self.listWidget.addItems(self.data)
+
+    # def click(self):
 
 
 if __name__ == '__main__':
